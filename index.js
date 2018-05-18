3e3186e23f0e5e2b84ea522cfc6029f6dba2bb9b
Renan <reebrando@gmail.com>
2018-05-18 17:03:43 -0300

    Added firebase support.

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 var mqtt = require('mqtt');
+var admin = require("firebase-admin");
+
+var config = require("./firebase-config.json");
 var client = mqtt.connect('mqtt://iot.eclipse.org');
 
+admin.initializeApp({
+    credential: admin.credential.cert(config),
+    databaseURL: "mydatabase"
+});
+
 client.on('connect', function () {
-  console.log('Oh Glorious Day! I have connected to eclipse broker. ')
-  client.subscribe('fiapmob');
-  client.publish('fiapmob', 'New Inbox');
+
+    console.log('Oh Glorious Day! I have connected to eclipse broker. ')
+    client.subscribe('fiapmob')
+
 });
 
 client.on('message', function (topic, message) {
-  console.log(topic);
-  console.log(message.toString());
-  client.end();
+
+    console.log(topic)
+    console.log(message.toString())
+
+    client.end();
+
+    // sending push notification
+    admin.messaging().send({
+        data: message.toString(),
+        topic: 'myinbox'
+    })
+    .then((response) => {
+        console.log('pushed successfully! ', response)
+    })
+    .catch((error) => {
+        console.log('error while pushing: ', error)
+    });
+
 });
